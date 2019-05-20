d3e9899295dcf7c511291a7629dc95dc83d707ea
hutapeasalomo@gmail.com <hutapeasalomo@gmail.com>
2019-05-20 10:27:08 +0700

    Improve the application

--- app/src/main/java/com/example/belajaronlineapplication/main/matematika/MatematikaActivity.kt
+++ app/src/main/java/com/example/belajaronlineapplication/main/matematika/MatematikaActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import com.example.belajaronlineapplication.main.matematika.Lesson1.LatihanSoalActivity
 import com.example.belajaronlineapplication.R
 import com.example.belajaronlineapplication.main.matematika.Lesson1.VideoActivity
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_matematika.*
 
 class MatematikaActivity : AppCompatActivity() {
@@ -14,6 +15,7 @@ class MatematikaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_matematika)
+        fetchPicture()
 
         arrowBack.setOnClickListener{
             super.onBackPressed()
@@ -38,5 +40,28 @@ class MatematikaActivity : AppCompatActivity() {
             startActivity(videoIntent)
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
         }
+    }
+
+    private fun fetchPicture () {
+        //Exercise
+        Picasso.get().load("https://firebasestorage.googleapis.com/v0/b/belajaronlineapplication.appspot.com/o/images%2Ff725c5a7-d302-4934-8689-2305ea6e9cd4?alt=media&token=b91db3d6-03b3-4c04-8a1b-6770a66904af").into(bab1Latihan)
+//        Picasso.get().load("https://firebasestorage.googleapis.com/v0/b/belajaronlineapplication.appspot.com/o/images%2Ff725c5a7-d302-4934-8689-2305ea6e9cd4?alt=media&token=b91db3d6-03b3-4c04-8a1b-6770a66904af").into(bab2Latihan)
+//        Picasso.get().load("https://firebasestorage.googleapis.com/v0/b/belajaronlineapplication.appspot.com/o/images%2Ff725c5a7-d302-4934-8689-2305ea6e9cd4?alt=media&token=b91db3d6-03b3-4c04-8a1b-6770a66904af").into(bab3Latihan)
+//        Picasso.get().load("https://firebasestorage.googleapis.com/v0/b/belajaronlineapplication.appspot.com/o/images%2Ff725c5a7-d302-4934-8689-2305ea6e9cd4?alt=media&token=b91db3d6-03b3-4c04-8a1b-6770a66904af").into(bab4Latihan)
+
+
+        //Topic
+        Picasso.get().load("https://firebasestorage.googleapis.com/v0/b/belajaronlineapplication.appspot.com/o/images%2Fbook1.png?alt=media&token=df6c1fda-f42d-4c0e-97e5-85b8b7ecd005").into(bab1_1)
+//        Picasso.get().load("https://firebasestorage.googleapis.com/v0/b/belajaronlineapplication.appspot.com/o/images%2Fbook1.png?alt=media&token=df6c1fda-f42d-4c0e-97e5-85b8b7ecd005").into(bab1_2)
+//        Picasso.get().load("https://firebasestorage.googleapis.com/v0/b/belajaronlineapplication.appspot.com/o/images%2Fbook1.png?alt=media&token=df6c1fda-f42d-4c0e-97e5-85b8b7ecd005").into(bab1_3)
+
+//        Picasso.get().load("https://firebasestorage.googleapis.com/v0/b/belajaronlineapplication.appspot.com/o/images%2Fbook1.png?alt=media&token=df6c1fda-f42d-4c0e-97e5-85b8b7ecd005").into(bab2_1)
+//        Picasso.get().load("https://firebasestorage.googleapis.com/v0/b/belajaronlineapplication.appspot.com/o/images%2Fbook1.png?alt=media&token=df6c1fda-f42d-4c0e-97e5-85b8b7ecd005").into(bab2_2)
+//        Picasso.get().load("https://firebasestorage.googleapis.com/v0/b/belajaronlineapplication.appspot.com/o/images%2Fbook1.png?alt=media&token=df6c1fda-f42d-4c0e-97e5-85b8b7ecd005").into(bab2_3)
+//
+//        Picasso.get().load("https://firebasestorage.googleapis.com/v0/b/belajaronlineapplication.appspot.com/o/images%2Fbook1.png?alt=media&token=df6c1fda-f42d-4c0e-97e5-85b8b7ecd005").into(bab3_1)
+//        Picasso.get().load("https://firebasestorage.googleapis.com/v0/b/belajaronlineapplication.appspot.com/o/images%2Fbook1.png?alt=media&token=df6c1fda-f42d-4c0e-97e5-85b8b7ecd005").into(bab3_2)
+//        Picasso.get().load("https://firebasestorage.googleapis.com/v0/b/belajaronlineapplication.appspot.com/o/images%2Fbook1.png?alt=media&token=df6c1fda-f42d-4c0e-97e5-85b8b7ecd005").into(bab3_3)
+
     }
 }
