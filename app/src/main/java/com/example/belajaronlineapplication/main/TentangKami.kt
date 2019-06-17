421f9d2d713ba988110cca82ea1752494ed795dc
hutapeasalomo@gmail.com <hutapeasalomo@gmail.com>
2019-06-18 04:53:24 +0700

    Added tentang kami

---
+++ app/src/main/java/com/example/belajaronlineapplication/main/TentangKami.kt
@@ -0,0 +1,12 @@
+package com.example.belajaronlineapplication.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TentangKami : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tentang_kami)
+    }
+}
